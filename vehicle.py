97e76747126388aebed218909b3816036ad13335
Stivan Strahilov <114061545+stivistr@users.noreply.github.com>
2023-04-20 19:39:02 +0300

    Add files via upload

--- vehicle.py
+++ vehicle.py
@@ -1,13 +1,11 @@
 class Vehicle:
-    def __init__(self, mileage: int, max_speed=150):
-        self.mileage = mileage
-        self.max_speed = max_speed
-        self.gadgets = []
+    DEFAULT_FUEL_CONSUMTPION = 1.25
 
+    def __init__(self, fuel: float, horse_power: int):
+        self.fuel = fuel
+        self.horse_power = horse_power
+        self.fuel_consumption = self.DEFAULT_FUEL_CONSUMTPION
 
-car = Vehicle(20)
-print(car.max_speed)
-print(car.mileage)
-print(car.gadgets)
-car.gadgets.append('Hudly Wireless')
-print(car.gadgets)
+    def drive(self, kilometers: float):
+        if self.fuel - kilometers * self.fuel_consumption >= 0:
+            self.fuel -= kilometers * self.fuel_consumption
