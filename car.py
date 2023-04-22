f5ed1511b6cfce450dd0d9624db9c56c94abd7d8
Stivan Strahilov <114061545+stivistr@users.noreply.github.com>
2023-04-22 21:05:43 +0300

    Add files via upload

--- car.py
+++ car.py
@@ -1,12 +1,6 @@
-class Car:
-    def __init__(self, name: str, model: str, engine: str):
-        self.name = name
-        self.model = model
-        self.engine = engine
-
-    def get_info(self) -> str:
-        return f"This is {self.name} {self.model} with engine {self.engine}"
+from project.vehicle import Vehicle
 
 
-car = Car("Kia", "Rio", "1.3L B3 I4")
-print(car.get_info())
+class Car(Vehicle):
+    DEFAULT_FUEL_CONSUMTPION = 3
+
