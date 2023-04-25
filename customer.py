d2a118efb8a16989736afd5ab59ff3335e5c0012
Stivan Strahilov <114061545+stivistr@users.noreply.github.com>
2023-04-25 23:08:17 +0300

    Add files via upload

--- customer.py
+++ customer.py
@@ -1,10 +1,17 @@
 class Customer:
-    def __init__(self, name: str, age: int, id: int):
+    id = 1
+
+    def __init__(self, name: str, address: str, email: str):
         self.name = name
-        self.age = age
-        self.id = id
-        self.rented_dvds = []
+        self.address = address
+        self.email = email
+        self.id = self.get_next_id()
+
+        Customer.id += 1
+
+    @staticmethod
+    def get_next_id():
+        return Customer.id
 
     def __repr__(self):
-        return f"{self.id}: {self.name} of age {self.age} has {len(self.rented_dvds)} " \
-               f"rented DVD's ({', '.join(self.rented_dvds)})"
+        return f"Customer <{self.id}> {self.name}; Address: {self.address}; Email: {self.email}"
